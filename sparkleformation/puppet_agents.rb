b9452d0029d40675a3c24af862b876ef685ebbf4
Cameron Johnston <cameron@rootdown.net>
2016-04-26 11:45:23 -0600

    update puppet_agents template to install sensu_client via sparklepack registry

--- sparkleformation/puppet_agents.rb
+++ sparkleformation/puppet_agents.rb
@@ -2,16 +2,22 @@ SparkleFormation.new(:puppet_agents).load(:base, :compute).overrides do
 
   parameters.puppet_server_hostname.type 'String'
   parameters.puppet_security_group_id.type 'String'
+  parameters.rabbitmq_hostname.type 'String'
+  parameters.rabbitmq_password.type 'String'
 
-  dynamic!(:asg, :puppet_agent, :security_groups => ref!(:puppet_security_group_id))
+  dynamic!(
+    :asg,
+    :puppet_agent,
+    :security_groups => ref!(:puppet_security_group_id),
+    :configsets => [:default, :sensu]
+  )
 
   resources(:puppet_agent_launch_configuration) do
-    registry!(:configure_aws_hostname)
-    registry!(:configure_ntp)
     metadata('AWS::CloudFormation::Init') do
       _camel_keys_set(:auto_disable)
-      configSets do
-        default [ 'configure_aws_hostname', 'configure_ntp', 'install_puppet_agent' ]
+      configSets do |sets|
+        sets.default += [ 'configure_aws_hostname', 'configure_ntp', 'install_puppet_agent' ]
+        sets.sensu [ ]
       end
       install_puppet_agent do
         commands('00_install_puppet_agent') do
@@ -27,5 +33,11 @@ SparkleFormation.new(:puppet_agents).load(:base, :compute).overrides do
         end
       end
     end
+    registry!(:sensu_client,
+      :queue_password => ref!(:rabbitmq_password),
+      :rabbitmq_hostname => ref!(:rabbitmq_hostname)
+    )
+    registry!(:configure_aws_hostname)
+    registry!(:configure_ntp)
   end
 end
