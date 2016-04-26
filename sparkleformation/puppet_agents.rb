954a842213a9a63266b8331a79b5a808a6ee24f6
Cameron Johnston <cameron@rootdown.net>
2016-04-26 09:24:51 -0600

    update puppet_agents template to automate installation of ntp, puppet agent

--- sparkleformation/puppet_agents.rb
+++ sparkleformation/puppet_agents.rb
@@ -1,20 +1,31 @@
 SparkleFormation.new(:puppet_agents).load(:base, :compute).overrides do
 
-  dynamic!(:asg, :puppet_agent)
+  parameters.puppet_server_hostname.type 'String'
+  parameters.puppet_security_group_id.type 'String'
+
+  dynamic!(:asg, :puppet_agent, :security_groups => ref!(:puppet_security_group_id))
 
   resources(:puppet_agent_launch_configuration) do
-  #   metadata('AWS::CloudFormation::Init') do
-  #     _camel_keys_set(:auto_disable)
-  #     configSets do |sets|
-  #       sets.default sets.default.push('install_puppet')
-  #     end
-  #     install_puppet do
-  #       packages do
-  #         apt.set!('puppet', [])
-  #       end
-  #     end
-  #   end
     registry!(:configure_aws_hostname)
+    registry!(:configure_ntp)
+    metadata('AWS::CloudFormation::Init') do
+      _camel_keys_set(:auto_disable)
+      configSets do
+        default [ 'configure_aws_hostname', 'configure_ntp', 'install_puppet_agent' ]
+      end
+      install_puppet_agent do
+        commands('00_install_puppet_agent') do
+          command join!(
+            'curl -k https://', ref!(:puppet_server_hostname), ':8140/packages/current/install.bash | sudo bash'
+          )
+          test 'test ! -e /usr/local/bin/puppet'
+        end
+        commands('01_run_puppet_agent') do
+          command join!(
+            'puppet agent --server ', ref!(:puppet_server_hostname), ' --waitforcert 120 -D'
+          )
+        end
+      end
+    end
   end
-
 end
